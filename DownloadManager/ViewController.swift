57e4c21dead3dab175144863ee9ff0eefb61c60e
Victor Cruz <vcruz@mobileiron.com>
2017-07-10 13:23:26 -0700

    Code version 5 Implemented labels in UI

--- DownloadManager/ViewController.swift
+++ DownloadManager/ViewController.swift
@@ -19,31 +19,35 @@ class ViewController: UIViewController {
     @IBOutlet weak var statusLbl: UILabel!
     @IBOutlet weak var responsesTextView: UITextView!
     @IBOutlet weak var downloadButtonOutlet: UIButton!
+    @IBOutlet weak var urlLabel: UILabel!
     
     
     @IBAction func downloadButton(_ sender: UIButton) {
-        self.progressBar.isHidden = false
-        self.progressLbl.isHidden = false
-        self.downloadButtonOutlet.isEnabled = false
-        
         //let fileDownloadURL = URL(string: "https://www.dropbox.com/s/77lp9p7055ru733/iOScapture.zip?dl=1")
         //let fileDownloadURL = URL(string: "http://192.168.203.138/test.zip")
         let fileDownloadURL = URL(string: "https://www.dropbox.com/s/pz71oox28q1jfo4/iOScapture.appcomnfig.pcap?dl=1")
+        
+        self.progressBar.isHidden = false
+        self.progressLbl.isHidden = false
+        self.downloadButtonOutlet.isEnabled = false
+        self.urlLabel.text = "URL: \(String(describing: fileDownloadURL!)) "
+        self.statusLbl.text = "Status: Downloading"
+
         
         
         let destination: DownloadRequest.DownloadFileDestination = { _, _ in
             let documentsURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
             let fileURL: URL = documentsURL.appendingPathComponent("test.file")
+            self.appendDebugLog(logText: fileURL.absoluteString)
+            
             return (fileURL, [.createIntermediateDirectories, .removePreviousFile])
         }
         
         Alamofire.download(fileDownloadURL!, to: destination)
             .response { response in
-                DispatchQueue.main.async {
-                    print("test")
-                    self.responsesTextView.text = String(describing: response.request) + String(describing: response.response) + String(describing: response.temporaryURL) + String(describing: response.destinationURL) + String(describing: response.error)
-                }
                 
+                let debugText = String(describing: response.request) + "\n" + String(describing: response.response) + "\n" + String(describing: response.temporaryURL) + "\n" + String(describing: response.destinationURL) + "\n" + String(describing: response.error)
+                self.appendDebugLog(logText: debugText)
                 
                 print(response.request)
                 print(response.response)
@@ -51,6 +55,9 @@ class ViewController: UIViewController {
                 print(response.destinationURL)
                 print(response.error)
                 
+                if response.error != nil {
+                    self.statusLbl.text = "Status: Failed with error"
+        }
             }
             .downloadProgress(queue: DispatchQueue.global(qos: .utility)) { progress in
 
@@ -66,16 +73,23 @@ class ViewController: UIViewController {
             
             .responseJSON { response in
                 debugPrint(response)
+                self.appendDebugLog(logText: response.debugDescription)
             }
             
             .validate {request, response, temporaryURL, destinationURL in
                 self.downloadButtonOutlet.setTitle("Download Again", for: .normal)
                 self.downloadButtonOutlet.isEnabled = true
+                self.statusLbl.text = "Status: Downloaded"
                 
                 return .success
-                
         }
 
+    }
+    
+    func appendDebugLog(logText: String) {
+        DispatchQueue.main.async {
+            self.responsesTextView.text = self.responsesTextView.text + "\n" + logText
+        }
     }
     
 
