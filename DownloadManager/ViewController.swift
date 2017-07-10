1f3cec9c375e4a46aa707f73b206cdae123adef9
Victor Cruz <vcruz@mobileiron.com>
2017-07-10 11:28:18 -0700

    fixed progress bar y progress label added logs added icon

--- DownloadManager/ViewController.swift
+++ DownloadManager/ViewController.swift
@@ -18,14 +18,18 @@ class ViewController: UIViewController {
     @IBOutlet weak var progressLbl: UILabel!
     @IBOutlet weak var statusLbl: UILabel!
     @IBOutlet weak var responsesTextView: UITextView!
+    @IBOutlet weak var downloadButtonOutlet: UIButton!
     
     
     @IBAction func downloadButton(_ sender: UIButton) {
-//        self.progressBar.isHidden = false
-//        self.progressLbl.isHidden = false
-        let mySelf = self
+        self.progressBar.isHidden = false
+        self.progressLbl.isHidden = false
+        self.downloadButtonOutlet.isEnabled = false
         
-        let fileDownloadURL = URL(string: "https://www.dropbox.com/s/77lp9p7055ru733/iOScapture.zip?dl=1")
+        //let fileDownloadURL = URL(string: "https://www.dropbox.com/s/77lp9p7055ru733/iOScapture.zip?dl=1")
+        //let fileDownloadURL = URL(string: "http://192.168.203.138/test.zip")
+        let fileDownloadURL = URL(string: "https://www.dropbox.com/s/pz71oox28q1jfo4/iOScapture.appcomnfig.pcap?dl=1")
+        
         
         let destination: DownloadRequest.DownloadFileDestination = { _, _ in
             let documentsURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
@@ -35,23 +39,39 @@ class ViewController: UIViewController {
         
         Alamofire.download(fileDownloadURL!, to: destination)
             .response { response in
-                self.responsesTextView.text = String(describing: response.temporaryURL)
+                DispatchQueue.main.async {
+                    print("test")
+                    self.responsesTextView.text = String(describing: response.request) + String(describing: response.response) + String(describing: response.temporaryURL) + String(describing: response.destinationURL) + String(describing: response.error)
+                }
+                
+                
                 print(response.request)
                 print(response.response)
                 print(response.temporaryURL)
                 print(response.destinationURL)
                 print(response.error)
+                
             }
             .downloadProgress(queue: DispatchQueue.global(qos: .utility)) { progress in
-                var dataProgress = (progress.fractionCompleted * 100)
-                //print(dataProgress)
-                mySelf.progressLbl.text = "Victor"
-                mySelf.progressBar.setProgress(0.5, animated: true)
-                //print("Progress: \(progress.fractionCompleted * 100)")
-                //print(String("\(progress.fractionCompleted * 100) %") as Any)
+
+                // Send the tread to the foreground (need to do this will all closures because closures runs in the background)
+                DispatchQueue.main.async {
+                    let progressText = String.init(format: "%0.2f %%", progress.fractionCompleted * 100)
+                    self.progressLbl.text = progressText
+                    self.progressBar.setProgress(Float(progress.fractionCompleted), animated: true)
+                }
+                print("Progress: \(progress.fractionCompleted * 100)")
+             
+            }
+            
+            .responseJSON { response in
+                debugPrint(response)
             }
             
             .validate {request, response, temporaryURL, destinationURL in
+                self.downloadButtonOutlet.setTitle("Download Again", for: .normal)
+                self.downloadButtonOutlet.isEnabled = true
+                
                 return .success
                 
         }
@@ -70,10 +90,4 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    //func DownloadFile(_ url: URL) -> Bool {
-        //Alamofire.download(url: url)
-    //}
-
 }
-
