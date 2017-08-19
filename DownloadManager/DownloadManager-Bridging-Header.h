18d788515f143e379e945eb3b1ca40dd007d02b3
Victor Cruz <vcruz@mobileiron.com>
2017-08-19 14:50:25 -0400

    added Objective-C Bridging Header and added cocoaPods DZEmptyDataSet

---
+++ DownloadManager/DownloadManager-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  DownloadManager-Bridging-Header.h
+//  DownloadManager
+//
+//  Created by Victor Cruz on 8/19/17.
+//  Copyright © 2017 MobileIron Professional Services. All rights reserved.
+//
+
+#ifndef DownloadManager_Bridging_Header_h
+#define DownloadManager_Bridging_Header_h
+
+
+#endif /* DownloadManager_Bridging_Header_h */
